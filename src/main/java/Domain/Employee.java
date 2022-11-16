fb265dacf94a2d564448787c008da75fbebdc918
M V K <krishna.mugala9@gmail.com>
2022-11-16 22:12:33 +0530

    created getter and setters for employee class

--- src/main/java/Domain/Employee.java
+++ src/main/java/Domain/Employee.java
@@ -18,4 +18,61 @@ public class Employee {
 
     public Employee() {
     }
+
+    public Employee(int employeeId, String employeeName, int employeeAge, String position, String address, Department employeeDepartment) {
+        this.employeeId = employeeId;
+        this.employeeName = employeeName;
+        this.employeeAge = employeeAge;
+        this.position = position;
+        Address = address;
+        this.employeeDepartment = employeeDepartment;
+    }
+
+    public int getEmployeeId() {
+        return employeeId;
+    }
+
+    public void setEmployeeId(int employeeId) {
+        this.employeeId = employeeId;
+    }
+
+    public String getEmployeeName() {
+        return employeeName;
+    }
+
+    public void setEmployeeName(String employeeName) {
+        this.employeeName = employeeName;
+    }
+
+    public int getEmployeeAge() {
+        return employeeAge;
+    }
+
+    public void setEmployeeAge(int employeeAge) {
+        this.employeeAge = employeeAge;
+    }
+
+    public String getPosition() {
+        return position;
+    }
+
+    public void setPosition(String position) {
+        this.position = position;
+    }
+
+    public String getAddress() {
+        return Address;
+    }
+
+    public void setAddress(String address) {
+        Address = address;
+    }
+
+    public Department getEmployeeDepartment() {
+        return employeeDepartment;
+    }
+
+    public void setEmployeeDepartment(Department employeeDepartment) {
+        this.employeeDepartment = employeeDepartment;
+    }
 }
